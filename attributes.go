6cddee5371e3e5dcb5ed14c8580a1d88ba70edaf
Vladimir Efremov <vefremov@gmail.com>
2019-08-31 23:20:41 +0200

    Getting (most part of) scalar attributes implemented

---
+++ attributes.go
@@ -0,0 +1,362 @@
+package p4db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Point represents a point in 3D space
+type Point struct {
+	X, Y, Z float64
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("Point{%g, %g, %g}", p.X, p.Y, p.Z)
+}
+
+// DRefPair represents pair of double (e.g. measured depth in a well) and reference to an object
+type DRefPair struct {
+	ValD float64
+	Ref  int64
+}
+
+func (p DRefPair) String() string {
+	return fmt.Sprintf("DRefPair{%g, %d}", p.ValD, p.Ref)
+}
+
+type Attribute interface {
+	String() string
+	AsInt() (int, error)
+	AsDouble() (float64, error)
+	AsRef() (int64, error)
+	AsPoint() (Point, error)
+	AsDRefPair() (DRefPair, error)
+	ToArray() (ArrayAttribute, bool)
+}
+
+type ArrayAttribute interface {
+	String() string
+	AsStringArr() ([]string, error)
+	AsIntArr() ([]int, error)
+	AsDoubleArr() ([]float64, error)
+	AsRefArr() ([]int64, error)
+	AsPointArr() ([]Point, error)
+	AsDRefPairArr() ([]DRefPair, error)
+}
+
+type dataValuesDB struct {
+	CodeValue     int64  `db:"CodeValue"`
+	LinkContainer int64  `db:"LinkContainer"`
+	LinkMetaData  int64  `db:"LinkMetaData"`
+	ValueIndex    int32  `db:"ValueIndex"`
+	Status        string `db:"Status"`
+}
+
+type dataValuesCDB struct {
+	dataValuesDB
+	DataValue sql.NullString `db:"DataValue"`
+}
+
+type dataValuesIDB struct {
+	dataValuesDB
+	DataValue int64 `db:"DataValue"`
+}
+
+type dataValuesDDB struct {
+	dataValuesDB
+	DataValue float64 `db:"DataValue"`
+}
+
+type dataValuesRDB struct {
+	dataValuesDB
+	DataValue int64 `db:"DataValue"`
+}
+
+type dataValuesPDB struct {
+	dataValuesDB
+	DataValueX float64 `db:"DataValueX"`
+	DataValueY float64 `db:"DataValueY"`
+	DataValueZ float64 `db:"DataValueZ"`
+}
+
+type dataValuesXDB struct {
+	dataValuesDB
+	DataValueD float64 `db:"DataValueD"`
+	DataValueR int64   `db:"DataValueR"`
+}
+
+// ========= C case =============
+
+type CAttr struct {
+	Val string
+}
+
+func (a CAttr) String() string {
+	return a.Val
+}
+
+func (a CAttr) AsInt() (int, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a CAttr) AsDouble() (float64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a CAttr) AsRef() (int64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+func (a CAttr) AsPoint() (Point, error) {
+	return Point{}, fmt.Errorf("conversion error")
+}
+func (a CAttr) AsDRefPair() (DRefPair, error) {
+	return DRefPair{}, fmt.Errorf("conversion error")
+}
+
+func (a CAttr) ToArray() (ArrayAttribute, bool) {
+	return nil, false
+}
+
+// ========= I case =============
+
+type IAttr struct {
+	Val int
+}
+
+func (a IAttr) String() string {
+	return fmt.Sprintf("%d", a.Val)
+}
+
+func (a IAttr) AsInt() (int, error) {
+	return a.Val, nil
+}
+
+func (a IAttr) AsDouble() (float64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a IAttr) AsRef() (int64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a IAttr) AsPoint() (Point, error) {
+	return Point{}, fmt.Errorf("conversion error")
+}
+func (a IAttr) AsDRefPair() (DRefPair, error) {
+	return DRefPair{}, fmt.Errorf("conversion error")
+}
+
+func (a IAttr) ToArray() (ArrayAttribute, bool) {
+	return nil, false
+}
+
+// ========= D case =============
+
+type DAttr struct {
+	Val float64
+}
+
+func (a DAttr) String() string {
+	return fmt.Sprintf("%g", a.Val)
+}
+
+func (a DAttr) AsInt() (int, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a DAttr) AsDouble() (float64, error) {
+	return a.Val, nil
+}
+
+func (a DAttr) AsRef() (int64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a DAttr) AsPoint() (Point, error) {
+	return Point{}, fmt.Errorf("conversion error")
+}
+func (a DAttr) AsDRefPair() (DRefPair, error) {
+	return DRefPair{}, fmt.Errorf("conversion error")
+}
+
+func (a DAttr) ToArray() (ArrayAttribute, bool) {
+	return nil, false
+}
+
+// ========= R case =============
+
+type RAttr struct {
+	Val int64
+}
+
+func (a RAttr) String() string {
+	return fmt.Sprintf("Ref:%d", a.Val)
+}
+
+func (a RAttr) AsInt() (int, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a RAttr) AsDouble() (float64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a RAttr) AsRef() (int64, error) {
+	return a.Val, nil
+}
+
+func (a RAttr) AsPoint() (Point, error) {
+	return Point{}, fmt.Errorf("conversion error")
+}
+func (a RAttr) AsDRefPair() (DRefPair, error) {
+	return DRefPair{}, fmt.Errorf("conversion error")
+}
+
+func (a RAttr) ToArray() (ArrayAttribute, bool) {
+	return nil, false
+}
+
+// ========= P case =============
+
+type PAttr struct {
+	ValX, ValY, ValZ float64
+}
+
+func (a PAttr) String() string {
+	return fmt.Sprintf("Point{%g, %g, %g}", a.ValX, a.ValY, a.ValZ)
+}
+
+func (a PAttr) AsInt() (int, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a PAttr) AsDouble() (float64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a PAttr) AsRef() (int64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a PAttr) AsPoint() (Point, error) {
+	return Point{a.ValX, a.ValY, a.ValZ}, nil
+}
+func (a PAttr) AsDRefPair() (DRefPair, error) {
+	return DRefPair{}, fmt.Errorf("conversion error")
+}
+
+func (a PAttr) ToArray() (ArrayAttribute, bool) {
+	return nil, false
+}
+
+// ========= X case =============
+
+type XAttr struct {
+	ValD float64
+	ValR int64
+}
+
+func (a XAttr) String() string {
+	return fmt.Sprintf("DRefPair{%g, %d}", a.ValD, a.ValR)
+}
+
+func (a XAttr) AsInt() (int, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a XAttr) AsDouble() (float64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a XAttr) AsRef() (int64, error) {
+	return 0, fmt.Errorf("conversion error")
+}
+
+func (a XAttr) AsPoint() (Point, error) {
+	return Point{}, fmt.Errorf("conversion error")
+}
+func (a XAttr) AsDRefPair() (DRefPair, error) {
+	return DRefPair{a.ValD, a.ValR}, nil
+}
+
+func (a XAttr) ToArray() (ArrayAttribute, bool) {
+	return nil, false
+}
+
+// ================ End of attribute type definitions
+
+func (db *P4db) ContainerScalarAttr(id int64, attrName string) (attr Attribute, err error) {
+	c, err := db.GetContainerById(id)
+	if err != nil {
+		return nil, err
+	}
+	ind, t, isAray, err := IndexByName(c.ContainerTypeStr, attrName)
+	if err != nil {
+		return nil, err
+	}
+	if isAray {
+		return nil, fmt.Errorf("a scalar attribute expected")
+	}
+	switch t {
+	case C:
+		log.Println("C case of attribute")
+		var v dataValuesCDB
+		if err = db.C.Get(&v, "select * from DataValuesC where LinkMetaData=? and LinkContainer=? and Status='Actual'", ind, id); err == nil {
+			attr = CAttr{Val: v.DataValue.String}
+			return
+		}
+	case I:
+		log.Println("I case of attribute")
+		var v dataValuesIDB
+		if err = db.C.Get(&v, "select * from DataValuesI where LinkMetaData=? and LinkContainer=? and Status='Actual'", ind, id); err == nil {
+			attr = IAttr{Val: int(v.DataValue)}
+			return
+		}
+	case D:
+		log.Println("D case of attribute")
+		var v dataValuesDDB
+		if err = db.C.Get(&v, "select * from DataValuesD where LinkMetaData=? and LinkContainer=? and Status='Actual'", ind, id); err == nil {
+			attr = DAttr{Val: v.DataValue}
+			return
+		}
+	case R:
+		log.Println("R case of attribute")
+		var v dataValuesRDB
+		if err = db.C.Get(&v, "select * from DataValuesR where LinkMetaData=? and LinkContainer=? and Status='Actual'", ind, id); err == nil {
+			attr = RAttr{Val: v.DataValue}
+			return
+		}
+	case P:
+		log.Println("P case of attribute")
+		var v dataValuesPDB
+		if err = db.C.Get(&v, "select * from DataValuesP where LinkMetaData=? and LinkContainer=? and Status='Actual'", ind, id); err == nil {
+			attr = PAttr{ValX: v.DataValueX, ValY: v.DataValueY, ValZ: v.DataValueZ}
+			return
+		}
+	case X:
+		log.Println("P case of attribute")
+		var v dataValuesXDB
+		if err = db.C.Get(&v, "select * from DataValuesX where LinkMetaData=? and LinkContainer=? and Status='Actual'", ind, id); err == nil {
+			attr = XAttr{ValD: v.DataValueD, ValR: v.DataValueR}
+			return
+		}
+	default:
+		log.Println("Unsupported attribute type")
+		panic("Unsupported attribute type " + t)
+	}
+	return nil, nil
+}
+
+func (db *P4db) ContainerArrayAttr(id int64, attrName string) (ArrayAttribute, error) {
+	return nil, nil
+}
+
+func (db *P4db) ContainerAttributes(id int64) (map[string]Attribute, error) {
+	log.Println(id)
+	res := make(map[string]Attribute)
+	res["path"] = CAttr{Val: "qwerwqer/qwerwqer/qwerewqr"}
+	return res, nil
+}
---
+++ attributes_test.go
@@ -0,0 +1,119 @@
+package p4db_test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/vaefremov/p4db"
+)
+
+func TestContainerScalarAttr(t *testing.T) {
+	db, err := p4db.Connect(DSN)
+	if err != nil {
+		panic(err)
+	}
+	var id int64 = 330087
+	val, err := db.ContainerScalarAttr(id, "path")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	res := val.String()
+	fmt.Printf("Attr value: %v\n", res)
+	if res != "test2_1188536112.19" {
+		t.Error()
+	}
+
+	// D case
+	id = 152290
+	if val, err = db.ContainerScalarAttr(id, "cdpStep"); err != nil {
+		t.Error(err)
+		return
+	}
+	if resD, err := val.AsDouble(); err == nil {
+		fmt.Printf("Attr value: %v\n", resD)
+		if resD != 12.5 {
+			t.Error()
+		}
+	} else {
+		t.Error(err)
+	}
+
+	// I case
+	id = 18521
+	if val, err = db.ContainerScalarAttr(id, "nx"); err != nil {
+		t.Error(err)
+		return
+	}
+	if resD, err := val.AsInt(); err == nil {
+		fmt.Printf("Attr value: %v\n", resD)
+		if resD != 191 {
+			t.Error()
+		}
+	} else {
+		t.Error(err)
+	}
+
+	// R case
+	id = 36474
+	if val, err = db.ContainerScalarAttr(id, "Ref2doc1"); err != nil {
+		t.Error(err)
+		return
+	}
+	if resD, err := val.AsRef(); err == nil {
+		fmt.Printf("Attr value: %v\n", resD)
+		if resD != 36475 {
+			t.Error()
+		}
+	} else {
+		t.Error(err)
+	}
+
+	// P case
+	id = 2201
+	if val, err = db.ContainerScalarAttr(id, "coords"); err != nil {
+		t.Error(err)
+		return
+	}
+	if resD, err := val.AsPoint(); err == nil {
+		exp := p4db.Point{955526.631944, 766664.251389, 0}
+		fmt.Printf("Attr value: %v\n", resD)
+		if math.Abs(resD.X-exp.X) > 1. || math.Abs(resD.Y-exp.Y) > 1. || math.Abs(resD.Z-exp.Z) > 1. {
+			t.Error(math.Abs(resD.X-exp.X), math.Abs(resD.Y-exp.Y), math.Abs(resD.Z-exp.Z))
+		}
+	} else {
+		t.Error(err)
+	}
+
+	// X case
+	id = 428394
+	if val, err = db.ContainerScalarAttr(id, "boundaries"); err != nil {
+		t.Error(err)
+		return
+	}
+	if resD, err := val.AsDRefPair(); err == nil {
+		exp := p4db.DRefPair{3406.48, 4467173}
+		fmt.Printf("Attr value: %v", resD)
+		if math.Abs(resD.ValD-exp.ValD) > 1. || resD.Ref != exp.Ref {
+			t.Error(math.Abs(resD.ValD-exp.ValD), resD.Ref)
+		}
+	} else {
+		t.Error(err)
+	}
+
+}
+
+func ExampleContainerAttributes() {
+	db, err := p4db.Connect(DSN)
+	if err != nil {
+		panic(err)
+	}
+	var id int64 = 330087
+	if attrs, err := db.ContainerAttributes(id); err == nil {
+		fmt.Printf("%v", attrs)
+		// Output: qq
+	} else {
+		panic(err)
+	}
+}
